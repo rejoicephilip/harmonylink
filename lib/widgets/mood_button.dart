4fa08dc13dd34ef1c8a5d51db70e828d9397f699
rejoicephilip <rejoicephilip8@gmail.com>
2025-12-03 15:52:48 -0700

    mood button UI

--- lib/widgets/mood_button.dart
+++ lib/widgets/mood_button.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class MoodButton extends StatelessWidget{
+class MoodButton extends StatefulWidget{
   final String label;
   final String mood;
 
@@ -11,12 +11,59 @@ class MoodButton extends StatelessWidget{
   });
 
   @override
+  State<MoodButton> createState() => _MoodButtonState();
+}
+
+class _MoodButtonState extends State<MoodButton> {
+  double _scale = 1.0;
+
+  void _setScale(bool down) {
+    void _setScale (bool down) {
+      if (down == true) {
+      _scale = 0.95;
+    } else {
+      _scale = 1.0;
+    }
+    
+    setState(() {});
+      }
+  }
+
+  void _goToCreate(){
+    Navigator.pushNamed(
+      context,
+      '/createPlaylist',
+      arguments: widget.mood,
+    );
+  }
+
+  @override 
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        Navigator.pop(context, mood);
-      },
-       child: Text(label),
-       );
+    return AnimatedScale(
+      scale: _scale,
+      duration: const Duration(
+        milliseconds: 120
+      ),
+      child: InkWell(
+      onTap: _goToCreate,
+      onTapDown: (_) => _setScale(true),
+      onTapUp: (_) => _setScale(false),
+      onTapCancel: () => _setScale(false),
+      borderRadius: BorderRadius.circular(12),
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          color: Theme.of(context).colorScheme.primaryContainer,
+        ),
+        child: Text(
+          widget.label,
+          textAlign: TextAlign.center,
+          style: const TextStyle(fontSize: 16),
+        ),
+      ),
+      ),
+    );
   }
 }
